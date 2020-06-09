5a2285f4ea415ea296037016984e3aaca92a3e2b
zikin <rodrigoor1999@outlook.com>
2020-06-08 19:55:38 -0500

    Major changes read full message
    Hashing algorithm donaterd by zikin.
    Huffman compression development started, freqTables and lists were implemented.
    Insertion sort renamed for dev purposes.
    Quicksort renamedd for dev purposes.
    Huffman tree structure was upgraded, it now contains methods for
    	merging.

---
+++ FileCompSys/Algorithms/HuffmanCompression/__init__.py
@@ -0,0 +1,6 @@
+import Algorithms
+from Algorithms.buildFreqHashTable import buildFreqHashTable
+
+freqTable = buildFreqHashTable("text.txt")
+for key in freqTable.keys():
+    print(key, freqTablekey[key])
---
+++ FileCompSys/DataStructures/HuffmanTree/__init__.py
@@ -0,0 +1,66 @@
+class node:
+    def __init__(self, freq, char = None):
+        self.freq = freq
+        self.left = None
+        self.right = None
+        self.char = char
+        self.cmpValue = "" ##Compressed value
+    def inLevelTraversal(self):
+        if self.left:
+            self.left.inLevelTraversal()
+        print(self.freq, end= ",")
+        if self.right:
+            self.right.inLevelTraversal()
+    # Returns the hufftable needed for decompresion, it is based on inlevelTraversal
+    def HuffmanTable(self,hufftable):
+        if self.char:
+            hufftable[self.cmpValue] =  self.char
+        else:
+            if self.left:
+                self.left.cmpValue = self.cmpValue + "0"
+                self.left.HuffmanTable(hufftable)
+            if self.right:
+                self.right.cmpValue = self.cmpValue + "1"
+                self.right.HuffmanTable(hufftable)
+
+class HuffTree:
+    def __init__(self, freq, char = None):
+        self.root = node(freq, char)
+    def  merge(self, hufftree):
+        newroot = node(self.root.freq + hufftree.root.freq)
+        newroot.left = self.root
+        newroot.right = hufftree.root
+        self.root = newroot
+    def inLevelTraversal(self):
+        if self.root:
+            self.root.inLevelTraversal()
+        else: return False
+    def HuffmanTable(self):
+        # Returns the hufftable needed for decompresion
+        huffTable = {}
+        if self.root:
+            self.root.HuffmanTable(huffTable)
+            return huffTable
+        else: return False
+    def toFile(self, filepath):
+        #stores the tree in a file
+        return True
+    def FromFile(filename):
+        #builds the tree from the path
+        return True
+
+
+
+###Samples
+if __name__ == "__main__":
+    a = HuffTree(5,"h")
+    b = HuffTree(3,"s")
+    c = HuffTree(1,"4")
+    c.merge(b)
+    c.merge(a)
+    print(c.root.freq)
+    c.inLevelTraversal()
+    HuffmanTable = c.HuffmanTable()
+    print()
+    for key in HuffmanTable.keys():
+        print(key, HuffmanTable[key])
