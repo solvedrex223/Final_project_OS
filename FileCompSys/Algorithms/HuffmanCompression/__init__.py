79a0bfa35565a75d28db868dd58156cca261e3a2
zikin <rodrigoor1999@outlook.com>
2020-06-09 21:39:54 -0500

    Huffman Compression now generates an uncoded binary file

--- FileCompSys/Algorithms/HuffmanCompression/__init__.py
+++ FileCompSys/Algorithms/HuffmanCompression/__init__.py
@@ -1,6 +1,27 @@
-import Algorithms
-from Algorithms.buildFreqHashTable import buildFreqHashTable
 
-freqTable = buildFreqHashTable("text.txt")
-for key in freqTable.keys():
-    print(key, freqTablekey[key])
+import Algorithms.FrequencyHashTable as Freq
+import DataStructures.HuffmanTree as Ht
+import Algorithms.InsertionSort as Is
+import Algorithms.FileCompression as Fp
+import Algorithms.binarizeTable as BT
+filepath = "../words_alpha.txt"
+freqTable = Freq.buildFreqHashTable(filepath)
+freqList = []
+for key in freqTable.keys(): freqList.append(Ht.HuffTree(freqTable[key], key))
+del freqTable
+freqList.sort(key=lambda x: x.root.freq, reverse=True)
+while len(freqList)>1:
+    freqList[-1].merge(freqList[-2])
+    temptree = freqList[-1]
+    freqList.pop()
+    freqList.pop()
+    freqList.append(temptree)
+    Is.insertionSort(freqList)
+huffTree = freqList[0]
+del freqList
+print(huffTree.root.freq)
+compTable = huffTree.CompressionTable()
+for key in compTable.keys(): print(key,compTable[key])
+compTable = BT.binarize(compTable)
+for key in compTable.keys(): print(key,compTable[key])
+Fp.FileCompression(filepath,compTable)
