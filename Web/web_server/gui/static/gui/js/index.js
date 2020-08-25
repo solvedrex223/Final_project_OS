9bd53983893f95c0ab958ca438bef981fd96bf93
SolvedRex <jeep.escobedo@gmail.com>
2020-08-25 15:16:16 -0500

    User logins using SQLite

---
+++ Web/web_server/gui/static/gui/js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $("#check").click(function (e) { 
+        $.ajax({
+            type: "GET",
+            url: "check_user",
+            data: {
+                'user' : $("#user").val(),
+                'password' : $("#password").val(),
+            },
+            dataType: "json",
+            success: function (response) {
+                if (response.check == 1) {
+                    $(location).attr(href, '/terminal');
+                }
+                else{
+                    alert("Usuario o contraseña incorrecta");
+                }
+            }
+        });        
+    });
+});
